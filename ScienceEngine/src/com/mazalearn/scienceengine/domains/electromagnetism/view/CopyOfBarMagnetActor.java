8e087c0d4dbf763c7f468567c3f0932c62a75402
Sridhar Sundaram <sridhar.sundaram@gmail.com>
2012-12-01 13:36:31 +0530

    reverting bar magnet changes for resistive touch - dont thnk it helped. preserved as Copyofbarmagnet just in case. some translations added.

--- ScienceEngine/src/com/mazalearn/scienceengine/domains/electromagnetism/view/CopyOfBarMagnetActor.java
+++ ScienceEngine/src/com/mazalearn/scienceengine/domains/electromagnetism/view/CopyOfBarMagnetActor.java
@@ -1,27 +1,29 @@
 package com.mazalearn.scienceengine.domains.electromagnetism.view;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.scenes.scene2d.EventListener;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.Array;
 import com.mazalearn.scienceengine.ScienceEngine;
 import com.mazalearn.scienceengine.core.model.Science2DBody;
-import com.mazalearn.scienceengine.core.view.IScience2DStage;
-import com.mazalearn.scienceengine.core.view.Parameter;
 import com.mazalearn.scienceengine.core.view.Science2DActor;
 import com.mazalearn.scienceengine.domains.electromagnetism.model.BarMagnet;
 import com.mazalearn.scienceengine.domains.electromagnetism.model.BarMagnet.Mode;
 
-public class BarMagnetActor extends Science2DActor {
+public class CopyOfBarMagnetActor extends Science2DActor {
   private final BarMagnet barMagnet;
   private BitmapFont font;
   private Vector2 newPos = new Vector2();
+  private Vector3 currentTouch = new Vector3();
+  private boolean drag = false;
   
-  public BarMagnetActor(Science2DBody body, TextureRegion textureRegion, 
+  public CopyOfBarMagnetActor(Science2DBody body, TextureRegion textureRegion, 
       BitmapFont font) {
     super(body, textureRegion);
     this.barMagnet = (BarMagnet) body;
@@ -34,6 +36,7 @@ public class BarMagnetActor extends Science2DActor {
     this.addListener(new ClickListener() {
       @Override
       public void touchUp(InputEvent event, float x, float y, int pointer, int button) {
+        drag = false;
         if (Mode.valueOf(barMagnet.getMode()) != Mode.Rotate) return;
         // Get negative of movement vector
         lastTouch.sub(event.getStageX(), event.getStageY());
@@ -52,26 +55,37 @@ public class BarMagnetActor extends Science2DActor {
       public boolean touchDown(InputEvent event, float localX, float localY, int pointer, int button) {
         if (!isAllowMove()) return false;
         lastTouch.set(event.getStageX(), event.getStageY());
+        if (Mode.valueOf(barMagnet.getMode()) != Mode.Rotate) {
+          drag = true;
+        }
         return true;
-     }
+      }
 
       @Override
       public void touchDragged(InputEvent event, float localX, float localY, int pointer) {
-        if (Mode.valueOf(barMagnet.getMode()) == Mode.Free) {
-          // Get negative of movement vector
-          lastTouch.sub(event.getStageX(), event.getStageY());
-          setPosition(getX() - lastTouch.x, getY() - lastTouch.y);
-          setPositionFromViewCoords(true);
-          ScienceEngine.selectParameter(Parameter.Move, (IScience2DStage) getStage());
-          // Recalibrate lastTouch to new coordinates
-          lastTouch.set(event.getStageX(), event.getStageY());
-        }
+        if (Mode.valueOf(barMagnet.getMode()) != Mode.Free) return;
+        // Get negative of movement vector
+        if (drag) moveToCurrent();
       }
     });
   }
 
+  private void moveToCurrent() {
+    // Screen coords of current touch
+    currentTouch.set(Gdx.input.getX(), Gdx.input.getY(), 0);
+    // Screen coords of current touch
+    getStage().getCamera().unproject(currentTouch);
+    // Get negative of movement vector
+    lastTouch.sub(currentTouch.x, currentTouch.y);
+    setPosition(getX() - lastTouch.x, getY() - lastTouch.y);
+    setPositionFromViewCoords(true);
+    // Recalibrate lastTouch to new coordinates
+    lastTouch.set(currentTouch.x, currentTouch.y);
+  }
+
   @Override
   public void draw(SpriteBatch batch, float parentAlpha) {
+    if (drag) moveToCurrent();
     super.draw(batch, parentAlpha);
     if (Mode.valueOf(barMagnet.getMode()) == Mode.Rotate) { // Display RPM
       drawRpm(batch, parentAlpha);
