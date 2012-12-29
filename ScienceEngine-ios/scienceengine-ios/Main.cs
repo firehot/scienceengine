5eb29e15bf7aca4a8b07f07e9ec3959a430b13d1
sridhar <sridhar.sundaram@gmail.com>
2012-12-29 16:46:15 +0530

    webviewcontroller

--- ScienceEngine-ios/scienceengine-ios/Main.cs
+++ ScienceEngine-ios/scienceengine-ios/Main.cs
@@ -1,22 +1,69 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 using MonoTouch.Foundation;
 using MonoTouch.UIKit;
+
 using com.badlogic.gdx.backends.ios;
 using com.mazalearn.scienceengine;
 
-namespace superjumperios
+namespace scienceengineios
 {		
+	public class IosPlatformAdapter : AbstractPlatformAdapter {
+		UIWindow window;
+		WebViewController webViewController;
+
+		public IosPlatformAdapter (UIWindow window, WebViewController webViewController): base( ) {
+			this.window = window;
+			this.webViewController = webViewController;
+		}
+		public override void browseURL (string url) {
+			string contentDirectoryPath = Path.Combine (NSBundle.MainBundle.BundlePath, "Content/");
+			webViewController.load(url);
+		//	webView.ScalesPageToFit = true;
+			// make the window visible
+			window.MakeKeyAndVisible ();
+		}
+	}
+
 	public class Application
 	{
 		[Register ("AppDelegate")]
 		public partial class AppDelegate : IOSApplication {
-			public AppDelegate(): base(new ScienceEngine(""), getConfig()) {
+			static UIWindow webViewWindow;
+			static ScienceEngine scienceEngine;
+			static WebViewController webViewController;
+			static UINavigationController navigationController;
 
+			public AppDelegate(): base(getScienceEngine(), getConfig()) {
 			}
+			public override bool FinishedLaunching (UIApplication app, NSDictionary options) {
+				bool result = base.FinishedLaunching(app, options);
 
+				// create a new window instance based on the screen size
+				webViewWindow = new UIWindow (UIScreen.MainScreen.Bounds);
+				/*UIWebView webView = new UIWebView(UIScreen.MainScreen.Bounds);
+				window.AddSubview(webView);
+				scienceEngine.setPlatformAdapter (new IosPlatformAdapter(window, webView));
+
+				return result;*/
+				webViewController = new WebViewController(app.KeyWindow);
+				
+				navigationController = new UINavigationController();
+				navigationController.PushViewController (webViewController, true);
+
+				// If you have defined a view, add it here:
+				//webViewWindow.AddSubview (navigationController.View);
+				webViewWindow.RootViewController = navigationController;
+				scienceEngine.setPlatformAdapter (new IosPlatformAdapter(webViewWindow, webViewController));
+				return result;
+			}
+			internal static ScienceEngine getScienceEngine () {
+				scienceEngine = new ScienceEngine ("");
+				return scienceEngine;
+			}
 			internal static IOSApplicationConfiguration getConfig() {
 				IOSApplicationConfiguration config = new IOSApplicationConfiguration();
 				config.orientationLandscape = true;
---
+++ ScienceEngine-ios/scienceengine-ios/WebViewController.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Drawing;
+using MonoTouch.UIKit;
+using MonoTouch.Foundation;
+
+namespace scienceengineios {
+	/// <summary>
+	/// Display web content with back/forward buttons, plus refresh and close
+	/// </summary>
+	public class WebViewController : UIViewController {
+		UIWebView webView;
+		UIToolbar navBar;
+		String url;
+		UIBarButtonItem [] items;
+		UIWindow mainWindow;
+
+		public WebViewController (UIWindow mainWindow) : base () {
+			this.mainWindow = mainWindow;
+		}
+		
+		public override void ViewDidLoad () {
+			base.ViewDidLoad ();
+			navBar = new UIToolbar ();
+			navBar.Frame = new RectangleF (0, View.Frame.Height-40, View.Frame.Width, 40);
+			navBar.TintColor = UIColor.DarkGray;			
+			
+			items = new UIBarButtonItem [] {
+				new UIBarButtonItem ("Back", UIBarButtonItemStyle.Bordered, (o, e) => { webView.GoBack (); }),
+				new UIBarButtonItem ("Forward", UIBarButtonItemStyle.Bordered, (o, e) => { webView.GoForward (); }),
+				new UIBarButtonItem (UIBarButtonSystemItem.FlexibleSpace, null),
+				new UIBarButtonItem (UIBarButtonSystemItem.Refresh, (o, e) => { webView.Reload (); }),
+				new UIBarButtonItem (UIBarButtonSystemItem.Stop, (o, e) => { 
+					webView.StopLoading ();
+					mainWindow.MakeKeyAndVisible();
+				})
+			};
+			navBar.Items = items;
+			
+			webView = new UIWebView ();
+			webView.Frame = new RectangleF (0, 0, View.Frame.Width, View.Frame.Height-40);
+			
+			webView.LoadStarted += delegate {
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = true;
+				navBar.Items[0].Enabled = webView.CanGoBack;
+				navBar.Items[1].Enabled = webView.CanGoForward;
+			};
+			webView.LoadFinished += delegate {
+				UIApplication.SharedApplication.NetworkActivityIndicatorVisible = false;
+				navBar.Items[0].Enabled = webView.CanGoBack;
+				navBar.Items[1].Enabled = webView.CanGoForward;
+			};
+			
+			webView.ScalesPageToFit = true;
+			webView.SizeToFit ();
+			webView.LoadRequest (new NSUrlRequest(new NSUrl(url)));
+			
+			navBar.AutoresizingMask = UIViewAutoresizing.FlexibleWidth | UIViewAutoresizing.FlexibleTopMargin;
+			webView.AutoresizingMask = UIViewAutoresizing.FlexibleDimensions;
+			
+			View.AddSubview (webView);
+			View.AddSubview (navBar);
+		}
+
+		public void load (String url)
+		{
+			if (webView != null) {
+				webView.LoadRequest (new NSUrlRequest (new NSUrl (url)));
+			}
+			this.url = url;
+		}
+		
+		public override bool ShouldAutorotateToInterfaceOrientation (UIInterfaceOrientation toInterfaceOrientation) {
+			return true;
+		}
+	}
+}
