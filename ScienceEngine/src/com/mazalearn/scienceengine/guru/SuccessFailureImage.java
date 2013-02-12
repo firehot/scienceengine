b6de80569ff51c9862e9fffcc470a1276d39ecc9
Sridhar Sundaram <sridhar.sundaram@gmail.com>
2013-02-12 10:31:34 +0530

    bugfixes

--- ScienceEngine/src/com/mazalearn/scienceengine/guru/SuccessFailureImage.java
+++ ScienceEngine/src/com/mazalearn/scienceengine/guru/SuccessFailureImage.java
@@ -4,20 +4,21 @@ import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.mazalearn.scienceengine.ScreenComponent;
 
-final class ScoreImage extends Image {
+final class SuccessFailureImage extends Image {
   private String message;
+  private final boolean success;
   private BitmapFont font;
 
-  ScoreImage(Texture texture, Skin skin) {
+  SuccessFailureImage(Texture texture, Skin skin, boolean success) {
     super(texture);
     this.setVisible(false);
     setSize(ScreenComponent.getScaledX(getWidth()), ScreenComponent.getScaledY(getHeight()));
+    this.success = success;
     font = skin.getFont("default-font");
   }
 
@@ -27,27 +28,27 @@ final class ScoreImage extends Image {
     this.setY(ScreenComponent.VIEWPORT_HEIGHT/2);
     this.message = message;
     this.setVisible(true);
+    float moveBy = ScreenComponent.getScaledY(success ? 10 : -10);
+    this.setRotation(-5f);
     this.addAction(
-          Actions.sequence(
-              Actions.alpha(0),
-              Actions.alpha(1, 1),
-              Actions.delay(2),
-              new Action() {
-                @Override
-                public boolean act(float delta) {
-                  setVisible(false);
-                  return true;
-                }
-              }
-          )
+        Actions.repeat(20,
+            Actions.sequence(
+                Actions.parallel(
+                    Actions.rotateBy(10f, 0.1f), 
+                    Actions.moveBy(0f, moveBy, 0.1f)),
+                Actions.parallel(
+                    Actions.rotateBy(-10f, 0.1f), 
+                    Actions.moveBy(0f, moveBy, 0.1f))
+                )
+            )
         );
   }
 
   public void draw(SpriteBatch batch, float parentAlpha) {
     super.draw(batch, parentAlpha);
     Color c = font.getColor();
-    font.setColor(Color.WHITE);
-    font.draw(batch, message, getX() + getWidth()/2 - 30, getY() + getHeight()/2);
+    font.setColor(Color.BLACK);
+    font.draw(batch, message, getX() + getWidth()/2 - 10, getY() + getHeight()/2);
     font.setColor(c);
   }
 }
